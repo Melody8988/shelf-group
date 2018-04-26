c598fbead376876d3e70eaac88d92db7cd71d4cd
David Kesler <thedavidkesler@gmail.com>
2018-04-26 14:05:01 -0500

    userinfo router setup and db tested

---
+++ server/routes/userinfo.router.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+/**
+ * Get all of the items on the shelf
+ */
+router.get('/', (req, res) => {
+// This route should return all users and their items
+    console.log('/userinfo GET route');
+    console.log('is authenticated?', req.isAuthenticated());
+    console.log('user', req.user);
+    if (req.isAuthenticated()) {
+        let queryText = `SELECT person.username, COUNT(item.id)
+        FROM person
+        LEFT JOIN item ON person.id=item.person_id
+        GROUP BY person.id
+        Order BY person.username;`;
+        pool.query(queryText).then((result) => {
+            res.send(result.rows);
+            console.log(result.rows);
+        }).catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
+    } else {
+        res.sendStatus(403);
+    }
+});//end router.get
+
+
+
+module.exports = router;

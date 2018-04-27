ba42a9fb9fcd508f65ba6bca2d45c3f7717376b0
Melody Massard <melody.massard8988@gmail.com>
2018-04-27 11:54:18 -0500

    combined functional

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,16 +1,21 @@
 import { combineReducers } from 'redux';
 import user from './userReducer';
 import login from './loginReducer';
-// import addItem from './addItemReducer';
-// import userInfo from './userInfoReducer';
+import addItem from './addItemReducer';
+import userInfo from './userInfoReducer';
 import getShelf from './shelfReducer'
 
 const store = combineReducers({
   user,
   login,
-  // addItem,
-  // userInfo
+  addItem,
+  userInfo,
   getShelf
+  // userinfo reducers
+
+  // additem reducers
+
+
 });
 
 export default store;
